1245b3911d0fc5b734169a1fdf9dd5c15b4d4f91
Marcel Simader <marcel0simader@gmail.com>
2021-03-07 17:14:09 +0100

    Added parameter support in ConsoleArguments class, fixed bugs, extended test_SEPIO

--- SEPModules/SEPIO.py
+++ SEPModules/SEPIO.py
@@ -16,27 +16,33 @@ from SEPModules.SEPPrinting import cl_p, CYAN
 class ConsoleArguments:
 	
 	#constants
-	__SET__ = "set"
-	__ARGS__ = "args"
-	__KWARGS__ = "kwargs"
-	__REQUIRED__ = "required"
-	__REQUIRED_AND_SET__ = "required and set"
+	SET_TOTAL        = "set"
+	SET_ARGS         = "args"
+	SET_KWARGS       = "kwargs"
+	SET_PARS         = "parameters"
+	REQUIRED         = "required"
+	REQUIRED_AND_SET = "required and set"
 	
 	#vars
 	_argnames, _kwargnames = None, None
 	requires_arg = {}
-	_args, _kwargs = {}, {}
+	_args, _kwargs, _pars = {}, {}, []
 	
 	def __init__(self, argnames, kwargnames, noLoad=False):		
 		#check if arg-names and kwarg-names are of type list
 		if not (type(argnames) is list and type(kwargnames) is list):
 			raise TypeError("Parameters 'arg-names' and 'kwarg-names' must be of type list (received {} and {}).".format(\
 														argnames.__class__.__name__, kwargnames.__class__.__name__))
+		
 		#check that arg-names and kwarg-names don't overlap
 		overlap = [(a, b) for a in argnames for b in kwargnames if a.replace(":", "") == b.replace("=", "")]
 		if len(overlap):
 			raise ValueError("Arguments and keyword arguments can not share the same name (args and kwargs {} overlap).".format(overlap))
-			
+		
+		#init vars in case noLoad is set
+		self.requires_arg = {}
+		self._args, self._kwargs, self._pars = {}, {}, []
+		
 		#load argument names into string
 		self._argnames = str()
 		for argname in argnames: self._argnames += argname
@@ -58,15 +64,14 @@ class ConsoleArguments:
 		"""
 		_args_in = getopt(sys.argv[1:], self._argnames, self._kwargnames)
 		
-		#check if parsing started with an argument
-		if len(_args_in[0]) < 1:
-			if len(_args_in[1]) < 1:
-				raise RuntimeError("Missing arguments: {} and {}.".format(argnames, kwargnames)) #all args missing
-			raise RuntimeError("Unexpected terminal argument '{}'.".format(_args_in[1][0])) #got unexpected argument
-		#check if parsing produced an error by having an argument after -options that did not expect one
-		if len(_args_in[1]) > 0:
-			raise RuntimeError("Expected no argument after '{}' but received '{}'.".format(_args_in[0][-1][0], _args_in[1][0]))
-			
+		#check if _args_in order is fishy by seeing if any parameter has a dash
+		if any(["-" in arg for arg in _args_in[1]]):
+			raise Exception("Argument or keyword argument '{}' not recognized. (Maybe the arguments are in the wrong order?)".format(\
+													_args_in[1][0]))
+		
+		#save parameters into self, preserving order
+		self._pars = _args_in[1]
+		
 		#iterate over and save the valid arguments and split them into args and kwargs
 		for arg, val in _args_in[0]:
 			if arg[:2] == "--":
@@ -100,22 +105,30 @@ class ConsoleArguments:
 			yield (key, self[key])
 	
 	@property
+	def pars(self):
+		for par in self._pars:
+			yield par
+	
+	@property
 	def size(self):
 		"""
 		Return either the amount of set args + keyword args, the amount of set args, the amount of set keyword args, the amount of required args,
 		or the amount of required and set args.
 		"""
-		result = {self.__SET__: len(self._args) + len(self._kwargs), \
-							self.__ARGS__: len(self._args), \
-							self.__KWARGS__: len(self._kwargs), \
-							self.__REQUIRED__: len([a for a in self.requires_arg.values() if a]), \
-							self.__REQUIRED_AND_SET__: len([a for a in self.requires_arg.items() if a[1] and a[0] in self])}
+		result = {self.SET_TOTAL: len(self._args) + len(self._kwargs), \
+							self.SET_ARGS: len(self._args), \
+							self.SET_KWARGS: len(self._kwargs), \
+							self.SET_PARS: len(self._pars), \
+							self.REQUIRED: len([a for a in self.requires_arg.values() if a]), \
+							self.REQUIRED_AND_SET: len([a for a in self.requires_arg.items() if a[1] and a[0] in self])}
 		
-		result.update({ "assigned": result[self.__SET__], \
-										"arg": result[self.__ARGS__], \
-										"kwarg": result[self.__KWARGS__], \
-										"req": result[self.__REQUIRED__], \
-										"required and assigned": result[self.__REQUIRED_AND_SET__], "req assigned": result[self.__REQUIRED_AND_SET__], "req set": result[self.__REQUIRED_AND_SET__]})
+		#add aliases
+		result.update({ "assigned": result[self.SET_TOTAL], \
+										"arg": result[self.SET_ARGS], \
+										"kwarg": result[self.SET_KWARGS], \
+										"pars": result[self.SET_PARS], \
+										"req": result[self.REQUIRED], \
+										"required and assigned": result[self.REQUIRED_AND_SET], "req assigned": result[self.REQUIRED_AND_SET], "req set": result[self.REQUIRED_AND_SET]})
 		return result
 	
 	def __contains__(self, options, all=True):
@@ -123,39 +136,55 @@ class ConsoleArguments:
 		Returns True if the options is found in args or kwargs.
 		'all' can be set to True for checking against all values of "type(options) == list/set" or to False for only checking if any match.
 		"""
-		isStr, isList, isDict = type(options) is str, type(options) is list, type(options) is dict
-		if not (isStr or isList or isDict):
-			raise TypeError("Illegal type '{}' for argument 'options' ('list', 'str' or 'dict' expected).".format(options.__class__.__name__))
+		isInt, isStr, isList, isDict = type(options) is int, type(options) is str, type(options) is list, type(options) is dict
+		if not (isInt or isStr or isList or isDict):
+			raise TypeError("Illegal type '{}' for argument 'options' ('int', 'str', 'list' or 'dict' expected).".format(options.__class__.__name__))
 		
-		intersection_min = int(all) * (len(options) - 1) #type checking guarantees options has a length dunder-method implemented
-		if isStr:
+		if not isInt: intersection_min = int(all) * (len(options) - 1) #type checking guarantees options has a length dunder-method implemented
+		
+		if isInt:
+			return options >= 0 and options < len(self._pars)
+		elif isStr:
 			return options in self._args.keys() or options in self._kwargs.keys() #check if key exists
 		elif isList:
-			return len(set(options) & set(self._args.keys())) + len(set(options) & set(self._kwargs.keys())) > intersection_min #check if all keys exist
+			return len(set(options) & set(self._args.keys())) + len(set(options) & set(self._kwargs.keys())) + len(set(options) & set(range(len(self._pars)))) > intersection_min #check if all keys exist
 		elif isDict:
-			return len([None for b in options.items() if b in self._args.items() or b in self._kwargs.items()]) > intersection_min #check if all keys exist and all values match
+			return len([None for b in options.items() if b in self._args.items() or b in self._kwargs.items() or b in [(i, par) for i, par in enumerate(self._pars)]]) > intersection_min #check if all keys exist and all values match
 	
 	def __repr__(self):
 		return "ConsoleArguments<(%s, %s)>" % ([a if b != ":" else a + ":" for a, b in zip(self._argnames, self._argnames[1:] + " ") if a != ":"], self._kwargnames)
 	
 	def __str__(self):
-		return "ConsoleArguments: {args: %s, kwargs: %s}" % (cl_p(self._args, CYAN), cl_p(self._kwargs, CYAN))
+		return "ConsoleArguments: (args: %s, kwargs: %s, pars: %s)" % (self._args, self._kwargs, self._pars)
 
 	def __getitem__(self, key):
+		"""
+		Returns an argument/keyword argument if the supplied key is a 'str' object. If the supplied key is of type 'int', the
+		corresponding parameter is returned. If the key is not found, 'None' is returned.
+		"""
 		#check type
-		if type(key) is not str:
-			raise TypeError("Key is of the wrong type (expected 'str', received '{}').".format(key.__class__.__name__))
+		if not (type(key) is str or type(key) is int):
+			raise TypeError("Key is of the wrong type (expected 'str' or 'int', received '{}').".format(key.__class__.__name__))
 		
-		if key in self._args.keys():
-			return True if self._args[key] == "" else self._args[key] #return true if arg option (i.e. flag) is set
-		elif key in self._kwargs.keys():
-			return self._kwargs[key]
-		else:
-			raise KeyError("'{}' was not found as argument or keyword argument.".format(key)) #throw key error if not in args or kwargs
-			
+		if type(key) is str:
+			if key in self._args.keys():
+				return True if self._args[key] 		== "" else self._args[key] #return true if arg option (i.e. flag) is set
+			elif key in self._kwargs.keys():
+				return True if self._kwargs[key] 	== "" else self._kwargs[key] #return true if kwarg option (i.e. flag) is set
+			else:
+				return None
+		else: #must be int
+			if (key >= 0 and key < len(self._pars)) or (key < 0 and key >= - len(self._pars)):
+				return self._pars[key]
+			else:
+				return None
+		
 	def __iter__(self):
+		"""
+		Enumerate args, then kwargs, then pars (in the form '(#order, par value, )').
+		"""
 		#combine args and kwargs
-		for key in list(self._args.keys()) + list(self._kwargs.keys()):
+		for key in list(self._args.keys()) + list(self._kwargs.keys()) + list(range(len(self._pars))):
 			yield (key, self[key])
 	
 
--- SEPModules/test/test_SEPIO.py
+++ SEPModules/test/test_SEPIO.py
@@ -8,6 +8,7 @@ class TestConsoleArgumentsMethods(unittest.TestCase):
 	#Set up a console arg manager with inputs:
 	#		args	: -a, -c 3, -d 				( -b   not set)
 	#		kwargs: --test=ok, --one=1 	(--two not set)
+	#		pars	: install
 	def setUp(self):
 		#setup empty console arg manager
 		self.CAM = ConsoleArguments([], [], noLoad=True)
@@ -15,10 +16,11 @@ class TestConsoleArgumentsMethods(unittest.TestCase):
 		self.CAM._argnames, self.CAM._kwargnames = "ab:c:d", ["test", "one=", "two"]
 		self.CAM.requires_arg = {"a": False, "b": True, "c": True, "d": False, "test": False, "one": True, "two": False}
 		self.CAM._args, self.CAM._kwargs = {"a":"", "c": "3", "d":""}, {"test": "ok", "one": "1"}
+		self.CAM._pars = ["install", "quit"]
 		
 	def tearDown(self):
 		del self.CAM
-		
+	
 	#++++TEST INIT++++
 	def test_cam_init_TypeError_ValueError(self):
 		with self.assertRaises(TypeError):
@@ -36,11 +38,12 @@ class TestConsoleArgumentsMethods(unittest.TestCase):
 		
 	#++++TEST PROPERTIES++++
 	def test_size_return(self):
-		self.assertEqual(self.CAM.size[ConsoleArguments.__SET__],	 						5)
-		self.assertEqual(self.CAM.size[ConsoleArguments.__ARGS__], 						3)
-		self.assertEqual(self.CAM.size[ConsoleArguments.__KWARGS__], 					2)
-		self.assertEqual(self.CAM.size[ConsoleArguments.__REQUIRED__], 				3)
-		self.assertEqual(self.CAM.size[ConsoleArguments.__REQUIRED_AND_SET__],2)
+		self.assertEqual(self.CAM.size[ConsoleArguments.SET_TOTAL],	 			5)
+		self.assertEqual(self.CAM.size[ConsoleArguments.SET_ARGS], 				3)
+		self.assertEqual(self.CAM.size[ConsoleArguments.SET_KWARGS], 			2)
+		self.assertEqual(self.CAM.size[ConsoleArguments.SET_PARS], 				2)
+		self.assertEqual(self.CAM.size[ConsoleArguments.REQUIRED], 				3)
+		self.assertEqual(self.CAM.size[ConsoleArguments.REQUIRED_AND_SET],2)
 		
 	#++++TEST DUNDER++++
 	def test_contains_TypeError(self):
@@ -48,30 +51,38 @@ class TestConsoleArgumentsMethods(unittest.TestCase):
 			True in self.CAM
 		
 	def test_contains_return(self):
+		with self.subTest(type='int'):
+			self.assertTrue (0 	in self.CAM)
+			self.assertFalse(-1 in self.CAM)
+			self.assertTrue (1 	in self.CAM)
+			self.assertFalse(2 	in self.CAM)
+		
 		with self.subTest(type='str'):
-			self.assertTrue("c" in self.CAM)
-			self.assertFalse("b" in self.CAM)
-			self.assertTrue("one" in self.CAM)
-			self.assertFalse("two" in self.CAM)
+			self.assertTrue ("c" 		in self.CAM)
+			self.assertFalse("b" 		in self.CAM)
+			self.assertTrue ("one" 	in self.CAM)
+			self.assertFalse("two" 	in self.CAM)
 		
 		with self.subTest(type='list'):
-			self.assertTrue(["a", "d", "one"] in self.CAM)
-			self.assertFalse(["a", "b", "one"] in self.CAM)
+			self.assertTrue (["a", "d", "one", 0] in self.CAM)
+			self.assertFalse(["a", "b", "one", 3] in self.CAM)
 		
 		with self.subTest(type='dict'):
-			self.assertTrue({"a":"", "c": "3", "test": "ok"} in self.CAM)
-			self.assertFalse({"a": "3", "c": "3", "one": "1"} in self.CAM)
+			self.assertTrue ({"a":"",   "c": "3", "test": "ok", 0: "install",   1: "quit"} 	in self.CAM)
+			self.assertFalse({"a": "3", "c": "3", "one": "1",   0: "uninstall", 2: "test"} 	in self.CAM)
 		
-	def test_getitem_KeyError_TypeError(self):
-		with self.assertRaises(KeyError):
-			self.CAM["what"]
+	def test_getitem_TypeError(self):
 		with self.assertRaises(TypeError):
-			self.CAM[3]
-			
+			self.CAM[["abc"]]
+		
 	def test_getitem_return(self):
-		self.assertTrue(self.CAM["a"])
-		self.assertEqual(self.CAM["c"], "3")
+		self.assertTrue (self.CAM["a"])
+		self.assertEqual(self.CAM["c"]   , "3")
 		self.assertEqual(self.CAM["test"], "ok")
+		self.assertEqual(self.CAM["Idk"] , None)
+		self.assertEqual(self.CAM[0]		 , "install")
+		self.assertEqual(self.CAM[1]		 , "quit")
+		self.assertEqual(self.CAM[2]		 , None)
 		
 	def test_iter(self):
 		for item in self.CAM:
