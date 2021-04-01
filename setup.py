9686b5452e1f4071fff57626e468e5550b29a20d
Marcel Simader <marcel0simader@gmail.com>
2021-04-01 21:22:35 +0200

    updates packaging

--- setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup, Extension
-
-setup(name="SEPModules",
-			version="0.0.1",
-			author="Marcel Simader",
-			author_email="marcel0simader@gmail.com",
-			packages=["SEPModules"])
